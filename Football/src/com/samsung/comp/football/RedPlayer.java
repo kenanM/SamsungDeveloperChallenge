79515c38e5768287b46dd8e67f2997fc7607a892
Kenan <kenan.mcgrath@gmail.com>
2013-01-06 23:05:27 +0000

    Added updated animation graphics and fixed bug whereby Computer controlled players were not being animated

--- Football/src/com/samsung/comp/football/RedPlayer.java
+++ Football/src/com/samsung/comp/football/RedPlayer.java
@@ -7,12 +7,12 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 public class RedPlayer extends Player {
 
 	private static final long serialVersionUID = 3L;
-	
-	public RedPlayer(float playerX, float playerY) {
-		super(playerX, playerY);
-		this.TEAM=TeamColour.RED;
-		hoverTexture = new Texture(Gdx.files.internal("red hover.png"));
-		walkSheet = new Texture(Gdx.files.internal("redPlayer.png"));
+
+	public RedPlayer(float x, float y) {
+		super(x, y);
+		this.TEAM = TeamColour.RED;
+		this.hoverTexture = new Texture(Gdx.files.internal("red hover.png"));
+		this.walkSheet = new Texture(Gdx.files.internal("redPlayer.png"));
 		this.walkAnimation = new Animation(0.025f,
 				createTextureRegion(walkSheet));
 		this.currentFrame = walkAnimation.getKeyFrame(0);
