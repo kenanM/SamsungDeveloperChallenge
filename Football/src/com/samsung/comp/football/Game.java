d5e070db5a8aa235992998b9cb346019060244d3
Kenan <kenan.mcgrath@gmail.com>
2013-01-07 01:05:31 +0000

    Added funny import statement yaaawn

--- Football/src/com/samsung/comp/football/Game.java
+++ Football/src/com/samsung/comp/football/Game.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
