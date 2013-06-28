e0ffd557fc71903cdff9528a3f6d0dc92474d004
Gavin Lam <cs09ggl@my.brunel.ac.uk>
2013-06-28 17:03:06 +0100

    Documentation on interface

--- Football/src/com/samsung/comp/football/Actions/Followable.java
+++ Football/src/com/samsung/comp/football/Actions/Followable.java
@@ -2,6 +2,11 @@ package com.samsung.comp.football.Actions;
 
 import com.badlogic.gdx.math.Vector2;
 
+/**
+ * This is used so a MoveToPosition can begin from either a ball or a player.
+ * 
+ */
+
 public interface Followable {
 	public Vector2 getPosition();
 }
